92615c28864180313137f98e8c9919b1a9bead75
Robert Marshall <robert@porta.network>
2022-08-09 22:49:06 +0700

    Initial Program Build

---
+++ 11 - Javascript - Create an NFT/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import MintNft from './MintNft';
+
+function App() {
+	return (
+		<div className="App">
+		<header className="App-header">
+			<MintNft />
+		</header>
+		</div>
+	);
+}
+
+export default App;
---
+++ 12 - Javascript - Wrap SOL/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import SendSol from './SendSol';
+
+function App() {
+	return (
+		<div className="App">
+		<header className="App-header">
+			<SendSol />
+		</header>
+		</div>
+	);
+}
+
+export default App;
